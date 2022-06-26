1f0270b111e03dba8afc7513a273b3d8af565c3e
angelDariaux <angeldariaux@gmail.com>
2022-06-26 16:12:47 -0600

    agregando los tipos de comparadores

---
+++ operadores.py
@@ -0,0 +1,36 @@
+# Comparador and
+
+es_estudiante = True
+trabaja = False
+
+print(es_estudiante and trabaja) # False
+
+# Comparador or
+
+print(es_estudiante or trabaja) # True
+
+# Comparador not
+
+print(not trabaja) # True
+
+
+
+numero1 = 5
+numero2 = 5
+numero3 = 7
+
+# Comparador de igualdad == 
+
+print(numero1 == numero2) # True
+
+# Comparador de negacion != 
+
+print(numero2 != numero3) # True
+
+# Tambien tenemos los demas operadores
+
+# > mayor que 
+# < menor que
+# >= mayor o igual que
+# <= menor o igual que
+
