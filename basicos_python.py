13aad393080b4dfe4d30b5741978ad94f7840c57
angelDariaux <angeldariaux@gmail.com>
2022-06-26 16:00:17 -0600

    convirtiendo los valores de una variable a otro tipo

--- basicos_python.py
+++ basicos_python.py
@@ -19,5 +19,22 @@ es_estudiante = True
 print("Es estudiante: ",es_estudiante)
 
 
+
           # ? Convertir un dato a un tipo diferente
 
+    # De string a numero 
+
+entrada1 = int(input('Escribe un numero: '))   # Este es el equivalente al prompt de javascript
+entrada2 = int(input('Escribe otro numero: '))  # todo: int() es el equivalente a parseInt()
+entrada = entrada1 + entrada2
+
+print(entrada)
+
+    # De decimal a entero
+
+numero_decimal = 4.5
+print(int(numero_decimal))
+
+    # De numero a string
+
+print(str(numero_decimal))   # todo: str() convierte los valores a string
