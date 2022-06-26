512b645c61824abcef8793ed48526a4e4e94c80f
angelDariaux <angeldariaux@gmail.com>
2022-06-26 15:48:59 -0600

    ejemplo de variables y modificado el readme del documento

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cursoDePython
+
+La shell intereactiva de python se abre escribiendo:
+`python3 `
+
+Para salir de ella hay 2 opciones la primera es:
+
+`ctrl + d` o bien `exit()`
---
+++ basicos_python.py
@@ -0,0 +1,23 @@
+          # ? Variables
+
+numero = 10
+numero2 = 16
+
+numero3 = numero + numero2
+
+print(numero3)
+
+
+nombre = 'Angel '   # hay un " " al final
+apellido = 'Hackerman'
+
+print(nombre + apellido)
+
+print(nombre * 4) # * Asi se puede multiplicar este string
+
+es_estudiante = True
+print("Es estudiante: ",es_estudiante)
+
+
+          # ? Convertir un dato a un tipo diferente
+
