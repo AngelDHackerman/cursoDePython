178933c235f5664f718f494c946499bd2fee5eb4
angelDariaux <angeldariaux@gmail.com>
2022-06-27 21:49:36 -0600

    aprendiendo a no repetir codigo con las funciones

--- funciones.py
+++ funciones.py
@@ -3,5 +3,19 @@ def imprimir_mensaje():   # * asi se escriben las funciones
   print('Estoy aprendiendo a usar funciones')
 
 imprimir_mensaje()
-imprimir_mensaje()
-imprimir_mensaje()
+
+
+
+def conversacion(mensaje):    # * Asi se pasan los parametros en python
+  print('Hola')
+  print('Como estas?')
+  print(mensaje)
+  print('Adios')
+
+opcion = int(input('Elige una opcion (1, 2, 3,): '))
+if opcion == 1:
+  conversacion('Elegiste la opcion 1')
+if opcion == 2:
+  conversacion('Elegiste la opcion 2')
+if opcion == 3:
+  conversacion('Elegiste la opcion 3')
