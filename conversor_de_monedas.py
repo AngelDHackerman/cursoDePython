88dc1405e5318a6b7d582d227dfa888f4d2bbdbf
angelDariaux <angeldariaux@gmail.com>
2022-06-27 21:23:24 -0600

    agregando la primera parte del nuevo conversor de monedas

--- conversor_de_monedas.py
+++ conversor_de_monedas.py
@@ -1,9 +1,41 @@
 # De quetzales a dolares
 
-quetzales = input('Cuantos quetzales tienes?: ')
+# quetzales = input('Cuantos quetzales tienes?: ')
+quetzales = 100
 quetzales = float(quetzales) # float convierte un string a un numero decimal
 valor_dolar = 7.78
 dolares = quetzales / valor_dolar
 dolares = round(dolares, 2) # el segundo parametro indica cuantos decimales queremos
 dolares = str(dolares)
 print('Tienes $' + dolares + ' Dolares')
+
+
+        # Pesos colombianos, argentinos y mexicanos
+
+menu = '''
+Bienvenido al conversor de monedas 
+
+1 - Pesos colombianos 
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opcion: '''
+
+opcion = int(input(menu))
+
+if opcion == 1:
+  pesos = input('Cuantos pesos colombianos tienes?: ')
+  pesos = float(pesos)
+  valor_dolar = 3875
+  dolares = pesos / valor_dolar
+  dolares = round(dolares, 2)
+  dolares = str(dolares)
+  print('Tienes $' + dolares + 'dolares')
+elif opcion == 2:
+  pass
+elif opcion == 3:
+  pass
+else:
+  print('Ingresa una opcion correcta por favor.')
+
+
