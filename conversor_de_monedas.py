bb9e8916ecac603c87cb36e89ea0132733bc2894
angelDariaux <angeldariaux@gmail.com>
2022-06-27 21:27:52 -0600

    terminado el nuevo conversor

--- conversor_de_monedas.py
+++ conversor_de_monedas.py
@@ -26,15 +26,27 @@ opcion = int(input(menu))
 if opcion == 1:
   pesos = input('Cuantos pesos colombianos tienes?: ')
   pesos = float(pesos)
-  valor_dolar = 3875
+  valor_dolar = 4132
   dolares = pesos / valor_dolar
   dolares = round(dolares, 2)
   dolares = str(dolares)
   print('Tienes $' + dolares + 'dolares')
 elif opcion == 2:
-  pass
+  pesos = input('Cuantos pesos argentinos tienes?: ')
+  pesos = float(pesos)
+  valor_dolar = 160
+  dolares = pesos / valor_dolar
+  dolares = round(dolares, 2)
+  dolares = str(dolares)
+  print('Tienes $' + dolares + 'dolares')
 elif opcion == 3:
-  pass
+  pesos = input('Cuantos pesos colombianos tienes?: ')
+  pesos = float(pesos)
+  valor_dolar = 20
+  dolares = pesos / valor_dolar
+  dolares = round(dolares, 2)
+  dolares = str(dolares)
+  print('Tienes $' + dolares + 'dolares')
 else:
   print('Ingresa una opcion correcta por favor.')
 
