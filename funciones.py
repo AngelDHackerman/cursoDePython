56452516d35323c47d4a2cb05c1202a6e6306a89
angelDariaux <angeldariaux@gmail.com>
2022-06-27 21:43:10 -0600

    escribiendo funciones en python

---
+++ funciones.py
@@ -0,0 +1,7 @@
+def imprimir_mensaje():   # * asi se escriben las funciones
+  print('Mensaje especial: ')
+  print('Estoy aprendiendo a usar funciones')
+
+imprimir_mensaje()
+imprimir_mensaje()
+imprimir_mensaje()
