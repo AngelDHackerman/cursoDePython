87b2daafaad6e7940d38163dfa11378c70b1cb1e
angelDariaux <angeldariaux@gmail.com>
2022-06-28 22:54:53 -0600

    mejorando el programa de los palindromos

--- palindromo.py
+++ palindromo.py
@@ -1,21 +1,21 @@
-def palindromo(palabra):
-  palabra = palabra.replace(' ', '') # Esto elimina los espacios de las oraciones
-  palabra = palabra.lower() # Hace todas las letras minusculas
-  palabra_invertida = palabra[::-1] # ? palabra[::-1] asi es como se invierten las palabras en python
-  if palabra == palabra_invertida:
-    return True
-  else:
-    return False
-                            # Siempre se deben dejar 2 espacios entre funciones en python
+def es_palindromo(palabra):
+    palabra = palabra.replace(' ', '').lower()
+    
+    if palabra[::] == palabra[::-1]: # palabra[::-1], asi se invierten las palabras en python
+        return True
+    
+    else:
+        return False
+
 
 def run():
-  palabra: input('Escribe una plabra: ')
-  es_palindromo = palindromo(palabra)
-  if es_palindromo == True:
-    print('Es un palindromo')
-  else:
-    print('No es palindromo')
+    palabra = input('Ingrese una palabra: ')
+    if es_palindromo(palabra):
+        print('Es palindromo')
+
+    else:
+        print('No es palindromo')
 
 
-if __name__ == '__main__': # todo: Esto es el entry point de python, toda vez python se encuentra con esto, empieza a correr todo lo que esta debajo.
-  run()
+if __name__ == "__main__": #todo: esta linea es el entry point. Por aqui python va a comenzar a correr el codigo
+    run()
