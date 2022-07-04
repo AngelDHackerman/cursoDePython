7d4e484e4b48751bf42d8ab0feb84cdc6b1fbd6e
angelDariaux <angeldariaux@gmail.com>
2022-07-03 21:20:03 -0600

    usando el ciclo for en python

---
+++ for.py
@@ -0,0 +1,6 @@
+
+      # ? Ciclo for
+
+limite = 50
+for x in range(limite + 1):  # range(), es la forma de iterar en secuencias numericas, sino agregamos el +1, el bucle se detiene un numero abajo 
+  print(x)
