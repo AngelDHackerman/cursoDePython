f09501706bc72f8e97b846666dd9dcd6dfb42823
angelDariaux <angeldariaux@gmail.com>
2022-07-03 21:33:20 -0600

    aprendiendo a convertir rangos a array y mas ciclos for

--- for.py
+++ for.py
@@ -1,6 +1,17 @@
 
       # ? Ciclo for
 
-limite = 50
+for y in range(10):
+  print(y)
+
+
+limite = 30
 for x in range(limite + 1):  # range(), es la forma de iterar en secuencias numericas, sino agregamos el +1, el bucle se detiene un numero abajo 
+  print(x)
+
+
+a = list(range(30))   # ! Asi se convierte un rango en un array.
+print(a)
+
+for x in range(5, 20 + 1):    # ? range(5, 20 + 1) 5 es el valor INICIAL  y 20 es el valor FINAL
   print(x)
